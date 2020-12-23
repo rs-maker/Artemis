73be54dde1d22beeba12c6177b84c0e85c3ead1c
Nicolas Ruscher <nicolas.ruscher@tum.de>
2020-12-23 14:18:12 +0100

    fix dashboard submission not shown

--- src/main/webapp/app/exercises/shared/dashboards/tutor/exercise-assessment-dashboard.component.ts
+++ src/main/webapp/app/exercises/shared/dashboards/tutor/exercise-assessment-dashboard.component.ts
@@ -14,7 +14,7 @@ import { ModelingExercise } from 'app/entities/modeling-exercise.model';
 import { UMLModel } from '@ls1intum/apollon';
 import { ComplaintService } from 'app/complaints/complaint.service';
 import { Complaint } from 'app/entities/complaint.model';
-import { getLatestSubmissionResult, Submission, SubmissionExerciseType } from 'app/entities/submission.model';
+import { getLatestSubmissionResult, setLatestSubmissionResult, Submission, SubmissionExerciseType } from 'app/entities/submission.model';
 import { ModelingSubmissionService } from 'app/exercises/modeling/participate/modeling-submission.service';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -313,6 +313,8 @@ export class ExerciseAssessmentDashboardComponent implements OnInit, AfterViewIn
                 // Set the received submissions. As the result component depends on the submission we nest it into the participation.
                 this.submissions = submissions.map((submission) => {
                     submission.participation!.submissions = [submission];
+                    setLatestSubmissionResult(submission, getLatestSubmissionResult(submission));
+                    console.log('debug: submission: ' + submission);
                     return submission;
                 });
                 //         setLatestResult(submissions);
