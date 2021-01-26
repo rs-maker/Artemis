c058c85939d0106c80967a4396df82ae2441dec0
René Lalla <r.lalla@gmx.de>
2021-01-26 20:22:09 +0100

    fix guided-tour

--- src/main/webapp/app/guided-tour/guided-tour.component.ts
+++ src/main/webapp/app/guided-tour/guided-tour.component.ts
@@ -492,7 +492,7 @@ export class GuidedTourComponent implements AfterViewInit, OnDestroy {
      * @return style object for the rectangle beside the highlighted element
      */
     public getOverlayStyle(position: OverlayPosition) {
-        let style;
+        let style: { [klass: string]: any } = {};
 
         if (this.selectedElementRect) {
             const selectedElementTop = this.selectedElementRect.top - this.getHighlightPadding();
