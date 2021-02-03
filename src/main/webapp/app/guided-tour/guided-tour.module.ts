2808b60b06ae10d36cc464d0d8405973cf20c591
Ivan Chimeno <chimeno.ivan@gmail.com>
2021-02-03 11:53:28 +0100

    import pipe

--- src/main/webapp/app/guided-tour/guided-tour.module.ts
+++ src/main/webapp/app/guided-tour/guided-tour.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { GuidedTourComponent } from './guided-tour.component';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { ArtemisMarkdownModule } from 'app/shared/markdown.module';
 
 @NgModule({
     declarations: [GuidedTourComponent],
-    imports: [ArtemisSharedModule],
+    imports: [ArtemisSharedModule, ArtemisMarkdownModule],
     exports: [GuidedTourComponent],
     entryComponents: [GuidedTourComponent],
     providers: [DeviceDetectorService],
