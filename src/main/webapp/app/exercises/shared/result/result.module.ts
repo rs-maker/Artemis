4104e05da1f396a3dcc10c17803d9971e15e52ff
Robert Schauer <ga94bit@mytum.de>
2021-04-22 15:14:20 +0200

    trying it without angular material again

--- src/main/webapp/app/exercises/shared/result/result.module.ts
+++ src/main/webapp/app/exercises/shared/result/result.module.ts
@@ -9,10 +9,10 @@ import { ResultComponent } from 'app/exercises/shared/result/result.component';
 import { ResultDetailComponent } from 'app/exercises/shared/result/result-detail.component';
 import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
 import { FeedbackCollapseComponent } from 'app/exercises/shared/result/feedback-collapse.component';
-import { MatExpansionModule } from '@angular/material/expansion';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-    imports: [ArtemisSharedModule, MomentModule, ArtemisProgrammingExerciseActionsModule, ArtemisSharedComponentModule, MatExpansionModule],
+    imports: [ArtemisSharedModule, MomentModule, ArtemisProgrammingExerciseActionsModule, ArtemisSharedComponentModule, NgbCollapse],
     declarations: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent, SubmissionResultStatusComponent, FeedbackCollapseComponent],
     exports: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent, SubmissionResultStatusComponent],
 })
